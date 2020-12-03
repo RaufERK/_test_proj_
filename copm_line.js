bb14de60a8b657edd5e9bc605657ca65d0b121a6
Rauf Erk <erk.rauf@gmail.com>
2020-12-03 13:49:47 +0300

    line is redy

---
+++ copm_line.js
@@ -0,0 +1,12 @@
+console.log('Line is ok!!!!!');
+console.log('Line is ok!!!!!');
+console.log('Line is ok!!!!!');
+console.log('Line is ok!!!!!');
+console.log('Line is ok!!!!!');
+console.log('Line is ok!!!!!');
+console.log('Line is ok!!!!!');
+console.log('Line is ok!!!!!');
+console.log('Line is ok!!!!!');
+console.log('Line is ok!!!!!');
+console.log('Line is ok!!!!!');
+console.log('Line is ok!!!!!');
