1c7802cdf545aa99b6227932e452b7985f345af4
Rauf Erk <erk.rauf@gmail.com>
2020-12-03 13:36:28 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,25 @@
+let str0 = 'Hello Wolds';
+let str1 = '0123456789';
+
+// console.log(str.includes('yd'));
+
+// console.log(str.indexOf('3'));
+// console.log(str.indexOf('y'));
+// console.log(str.lastIndexOf('y'));
+
+
+// console.log(str1.slice(5,-2));
+// console.log(str1.substring(2));
+
+// перевернуь строку тремя методами
+// let rvr = str0.split('').reverse().join('')
+// let rvr1 = [...str0].reverse().join('')
+
+// console.log(rvr.toLowerCase());
+// console.log(rvr1.toLocaleUpperCase()+'!!!!');
+
+//regex101.com
+// "a".charCodeAt(0)
+
+// console.log('ABC'.charCodeAt(3))
+
